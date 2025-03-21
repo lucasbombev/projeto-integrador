46e15c5622dae4c92eab656c0bfc329c4aa79e7e
Lucas PC PopOS! <olucasbm1213@gmail.com>
2025-03-20 22:01:05 -0300

    home bufix

--- c-library/clibrary/clibrary/urls.py
+++ c-library/clibrary/clibrary/urls.py
@@ -1,8 +1,10 @@
 
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('', views.hello_world, name='hello_world'),
     path('admin/', admin.site.urls),
     path('livros/', include('livros.urls')),
 ]
